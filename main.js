5193a0c17577d75b1053f04fb84814adafebc0fc
Mostafa Kerim <mostafakerim30@gmail.com>
2019-10-15 07:55:23 +0200

    Color to the MOTD and made server replay packets
    Server replays many but not all packets to prevent unrelevant packets from kicking the client

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // imports
 const mc = require('minecraft-protocol'); // to handle minecraft login session
 const webserver = require('./webserver.js'); // to serve the webserver
@@ -30,10 +29,10 @@ function stop(){
 	webserver.isInQueue = false;
 	webserver.queuePlace = "None";
 	webserver.ETA = "None";
-	client.end(); // disconnect
 	if (proxyClient) {
 		proxyClient.end("Stopped the proxy."); // boot the player from the server
 	}
+	client.end(); // disconnect
 	server.close(); // close the server
 }
 
@@ -48,15 +47,47 @@ function startQueuing() {
 		version: config.MCversion
 	});
 	let finishedQueue = false;
+	let packetMetaArr = [];
+	let packetDataArr = [];
+	let packetExceptions = ["encryption_begin", "compress", "custom_payload", "success", "chat", "compress", "playerlist_header", "keep_alive", "update_time"];
+	let firstPackets = true
 	client.on("packet", (data, meta) => { // each time 2b2t sends a packet
 		if (!finishedQueue && meta.name === "playerlist_header") { // if the packet contains the player list, we can use it to see our place in the queue
 			let headermessage = JSON.parse(data.header);
-			let positioninqueue = headermessage.text.split("\n")[5].substring(25);
 			let ETA = headermessage.text.split("\n")[6].substring(27);
-			webserver.queuePlace = positioninqueue; // update info on the web page
 			webserver.ETA = ETA;
-			server.motd = `Place in queue: ${positioninqueue}`; // set the MOTD because why not
+			
+			let positioninqueue = headermessage.text.split("\n")[5].substring(25);
+			webserver.queuePlace = positioninqueue; // update info on the web page
+			positioninqueue = parseInt(positioninqueue, 10);
+			if (positioninqueue >= 300) {
+				server.motd = `§6§lPlace in queue: §c${positioninqueue}`;
+			} else if (positioninqueue >= 200) {
+				server.motd = `§6§lPlace in queue: §e${positioninqueue}`;
+			} else if (positioninqueue >= 100) {
+				server.motd = `§6§lPlace in queue: §a${positioninqueue}`;
+			} else if (positioninqueue < 100) {
+				server.motd = `§6§lPlace in queue: §2${positioninqueue}`;
+			} else {
+				server.motd = `§6§lPlace in queue: §6None`;
+			}
 		}
+		
+		if (!finishedQueue && firstPackets && !(packetExceptions.indexOf(meta.name) > -1)) {
+			packetMetaArr.push(meta)
+			packetDataArr.push(data)
+		}
+		
+		if (meta.name === "update_time" && firstPackets) {
+			firstPackets = false;
+		
+			for (i = 0; i < packetMetaArr.length; i++) {
+				if (packetMetaArr[i].name != "map_chunk") {
+					console.log(`Meta: ${packetMetaArr[i].name}, Data: ${JSON.stringify(packetDataArr[i])}`);
+				}
+			}
+		}
+		
 		if (finishedQueue === false && meta.name === "chat") { // we can know if we're about to finish the queue by reading the chat message
 			// we need to know if we finished the queue otherwise we crash when we're done, because the queue info is no longer in packets the server sends us.
 			let chatMessage = JSON.parse(data.message);
@@ -80,11 +111,11 @@ function startQueuing() {
 	// set up actions in case we get disconnected.
 	client.on('end', () => {
 		if (proxyClient) {
-            proxyClient.end("Connection reset by 2b2t server.\nReconnecting...");
+            proxyClient.end("Connection reset by 2b2t server.");
             proxyClient = null
 		}
 		stop();
-		// setTimeout(startQueuing, 100); // reconnect after 100 ms
+		//setTimeout(startQueuing, 100); // reconnect after 100 ms
 	});
 
 	client.on('error', (err) => {
@@ -94,7 +125,7 @@ function startQueuing() {
 		}
 		console.log('err', err);
 		stop();
-		// setTimeout(startQueuing, 100); // reconnect after 100 ms
+		setTimeout(startQueuing, 100); // reconnect after 100 ms
 	});
 
 	server = mc.createServer({ // create a server for us to connect to
@@ -107,24 +138,11 @@ function startQueuing() {
 	});
 
 	server.on('login', (newProxyClient) => { // handle login
-		newProxyClient.write('login', {
-			entityId: newProxyClient.id,
-			levelType: 'default',
-			gameMode: 0,
-			dimension: 0,
-			difficulty: 2,
-			maxPlayers: server.maxPlayers,
-			reducedDebugInfo: false
-		});
-		newProxyClient.write('position', {
-			x: 0,
-			y: 1.62,
-			z: 0,
-			yaw: 0,
-			pitch: 0,
-			flags: 0x00
-		});
-
+		for (i = 0; i < packetMetaArr.length; i++) {
+			filterPacketAndSend(packetDataArr[i], packetMetaArr[i], newProxyClient);
+			sleep(2);
+		}
+		
 		newProxyClient.on('packet', (data, meta) => { // redirect everything we do to 2b2t
 			filterPacketAndSend(data, meta, client);
 		});
@@ -136,7 +154,16 @@ function startQueuing() {
 //function to filter out some packets that would make us disconnect otherwise.
 //this is where you could filter out packets with sign data to prevent chunk bans.
 function filterPacketAndSend(data, meta, dest) {
-	if (meta.name !="keep_alive" && meta.name !="update_time") { //keep alive packets are handled by the client we created, so if we were to forward them, the minecraft client would respond too and the server would kick us for responding twice.
+	if (meta.name != "keep_alive" && meta.name != "update_time") { //keep alive packets are handled by the client we created, so if we were to forward them, the minecraft client would respond too and the server would kick us for responding twice.
 		dest.write(meta.name, data);
 	}
+}
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
